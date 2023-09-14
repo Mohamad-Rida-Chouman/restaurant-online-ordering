fcb3e8e80c206a001b4104200598b5fb208eaed7
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-09-14 17:28:08 +0300

    <BE> Created Menu Routes

---
+++ server/routes/menu.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const menuController = require('../controllers/menu');
+
+router.get('/', menuController.getAllMenuItems);
+router.get('/:id', menuController.getMenuItemById);
+router.post('/', menuController.createMenuItem);
+router.put('/:id', menuController.updateMenuItem);
+router.delete('/:id', menuController.deleteMenuItem);
+
+module.exports = router;
